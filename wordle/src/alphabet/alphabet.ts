1ba48a769d570c29807c42e4e16f429302f5f5e5
Jenny Newton <jennynewton@live.co.uk>
2022-07-13 22:29:25 +0100

    Fix alphabet.ts errors, fix behaviour of updateAlphabetDisplay, remove comments and old code from main.ts

--- wordle/src/alphabet/alphabet.ts
+++ wordle/src/alphabet/alphabet.ts
@@ -43,22 +43,18 @@ export class Alphabet {
   }
 
   setCorrect(letter: Letters): void {
-    console.log(letter);
     this.state[letter] = "Y";
   }
-  // marks letter as correct
 
   setSemiCorrect(letter: Letters): void {
-    console.log(letter);
-    this.state[letter] = "P"; 
+    if (this.state[letter] !== "Y") {
+      this.state[letter] = "P"; 
+    }
   }
-  // marks letter as partially correct - ***note to self - check this is not overriding correct if someone inadvertantly moves their correct letter in their next guess***
 
   setIncorrect(letter: Letters): void {
-    console.log(letter);
     this.state[letter] = "N"; 
   }
-  // marks letter as incorrect
 
   updateAlphabetDisplay(): void {
     // Loop through keys in state. If value is Y, find <letter> with id letter-${key}, add class correct. 
@@ -80,10 +76,9 @@ export class Alphabet {
         currentClass = "incorrect";
       }
 
-      letterElement.className = "";
-      letterElement.classList.add(currentClass);
+      letterElement!.className = "";
+      letterElement!.classList.add(currentClass);
       console.log(document.getElementById(`${item[0].toLowerCase()}`));
-      // ***Note to self - classList.add is piling classes up in the letters, not swapping them! CHANGE THIS CODE***
     });
   }
 }
--- wordle/src/main.ts
+++ wordle/src/main.ts
@@ -4,17 +4,10 @@ import { Alphabet, Letters } from './alphabet/alphabet';
 export {};
   let words = ["daisy", "henge", "roots", "bathe", "shoes", "fluff", "dirty", "clean", "roads", "kitty", "tiger", "grass", "bench", "range", "acres", "blank", "walls", "viola", "words", "hives", "hover", "mouse", "honey", "scarf", "trees", "hares", "books", "tease", "zebra", "lolly"];
   
-  // const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-  // const alphabetIndex = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // starting to replace these with the below variable which will store state
   const alpha = new Alphabet();
 
   let chosenWord = words[Math.floor(Math.random() * words.length)];
   let guessNumber = 0;
-    /*
-  let alphabetString = alphabet.join('');
-  let alphabetDiv = <HTMLElement>document.querySelector("#alphabet");
-  alphabetDiv.innerHTML = alphabetString;
-  */
 
   let inputField = <HTMLInputElement>document.querySelector(".textfield");
 
@@ -58,13 +51,10 @@ export {};
           let guessArray = guess.toUpperCase().split("");
           let wordArray = word.toUpperCase().split("");
 
-          markCorrectMatches(guessArray, wordArray/*, boxes*/);
+          markCorrectMatches(guessArray, wordArray, boxes);
 
           markSemicorrectMatches(guessArray, wordArray, boxes);
-        /*
-          alphabetString = alphabet.join('');
-          alphabetDiv.innerHTML = alphabetString;
-        */
+
           alpha.updateAlphabetDisplay();
           inputField.value = "";
           guessNumber++;
@@ -72,12 +62,11 @@ export {};
 
   }
 
-  function markCorrectMatches(guess: string[], word: string[], /*boxes: NodeListOf<HTMLElement> */) {
+  function markCorrectMatches(guess: string[], word: string[], boxes: NodeListOf<HTMLElement> ) {
       guess.forEach(function (letter, index, guess) {
           if (letter === word[index]) {
                 alpha.setCorrect(<Letters>letter);
-              // alphabet[alphabetIndex.indexOf(letter.toUpperCase())] = `<span class=\"correct\">${letter}</span>`; // this line marks letter as correct visually. We want to avoid this HTML stuff. Going to just log state of letter instead. 
-              // boxes[index].classList.add("correct"); // replace this with function call updateAlphabetDisplay.
+              boxes[index].classList.add("correct"); 
               word[index] = "_";
               guess[index] = ".";
           }
@@ -88,7 +77,6 @@ export {};
       guess.forEach(function (letter, index, guess) {
           if (word.indexOf(letter) !== -1) {
                 alpha.setSemiCorrect(<Letters>letter); // Casting using <Letters> - this only helps at compile time! Is gone by the time the code is run. This is why casting is bad!
-              // alphabet[alphabet.indexOf(letter.toUpperCase())] = `<span class=\"halfcorrect\">${letter}</span>`;
               boxes[index].classList.add("halfcorrect");
               word[word.indexOf(letter.toUpperCase())] = "_";
               guess[index] = ".";
@@ -96,7 +84,6 @@ export {};
           else if (letter !== ".") {
                 alpha.setIncorrect(<Letters>letter);
               boxes[index].classList.add("incorrect");
-              // alphabet[alphabet.indexOf(letter.toUpperCase())] = `<span class=\"incorrect\">${letter}</span>`;
           }
       });
   }
