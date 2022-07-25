39094fda0de873815a05730b6b6c1429f5cf792d
Jenny Newton <jennynewton@live.co.uk>
2022-07-25 21:49:13 +0100

    Begin creating class for processing guess

--- wordle/src/main.ts
+++ wordle/src/main.ts
@@ -2,92 +2,109 @@ import './assets/scss/main.scss';
 import { Alphabet, Letters } from './alphabet/alphabet';
 import { words } from './words/words';
 
-export {};
-    
-  const alpha = Alphabet.create(); // This is called a factory method - creates a new thing from class. 
+export { };
 
-  let chosenWord = words[Math.floor(Math.random() * words.length)];
-  let guessNumber = 0;
+const alpha = Alphabet.create(); // This is called a factory method - creates a new thing from class. 
 
-    // HOMEWORK! Move the below variable and method into a module. Pass it the arrow function. 
-  let inputField = <HTMLInputElement>document.querySelector(".textfield");
+let chosenWord = words[Math.floor(Math.random() * words.length)];
+let guessNumber = 0;
 
-  inputField.addEventListener('keyup', (e) => {
- 
-      processGuess(inputField.value, e, chosenWord);
-  });
+// HOMEWORK! Move the below variable and method into a module. Pass it the arrow function. 
+let inputField = <HTMLInputElement>document.querySelector(".textfield");
 
-  function processGuess(guess: string, e: KeyboardEvent, word: string): void {
-      let displayLetters = document.querySelectorAll<HTMLElement>(`letter-block-${guessNumber} > letter-input`); 
-      let guessArray = guess.toUpperCase().split(""); // Deal with this as part of the above homework
+inputField.addEventListener('keyup', (e) => {
 
-      if (e.key === 'Enter' && guess.length === 5) {
-          matchLetters(guess, word, displayLetters);
-      }
+    processGuess(inputField.value, e, chosenWord);
+});
 
-      displayLetters.forEach(function(box, index) {
-          if (guessArray[index]) {
-              box.innerHTML = guessArray[index];
-          }
-          else {
-              box.innerHTML = " ";
-          }
-      });
-      
-  }
+function processGuess(guess: string, e: KeyboardEvent, word: string): void {
+    let displayLetters = document.querySelectorAll<HTMLElement>(`letter-block-${guessNumber} > letter-input`);
+    let guessArray = guess.toUpperCase().split(""); // Deal with this as part of the above homework
 
-// HOMEWORK! Do this first. Create a class 'guessProcessor' or something logical, and add those functions to it. 
+    if (e.key === 'Enter' && guess.length === 5) {
+        matchLetters(guess, word, displayLetters);
+    }
 
-  function matchLetters(guess: string, word: string, boxes: NodeListOf<HTMLElement>) {
-       
+    displayLetters.forEach(function (box, index) {
+        if (guessArray[index]) {
+            box.innerHTML = guessArray[index];
+        }
+        else {
+            box.innerHTML = " ";
+        }
+    });
 
-      console.log(word);
+}
 
-      if (guessNumber >= 6) {
-          inputField.setAttribute("readonly", "");
-          inputField.value = "No more guesses!";
-      }
-      
+// HOMEWORK! Do this first. Create a class 'GuessProcessor' or something logical, and add those functions to it. 
 
-      else {
+// Move this class to a module
+class GuessProcessor {
 
-          let guessArray = guess.toUpperCase().split("");
-          let wordArray = word.toUpperCase().split("");
+    // Need a method that checks if max number of guesses has been reached, or actually would this be done in the main.ts file?
 
-          markCorrectMatches(guessArray, wordArray, boxes);
+    markCorrectGuesses() {
+        // this is the markCorrectMatches function
+    }
 
-          markSemicorrectMatches(guessArray, wordArray, boxes);
+    markSemicorrectGuesses() {
+        // this is the markSemicorrectMatches function
+    }
 
-          alpha.updateAlphabetDisplay();
-          inputField.value = "";
-          guessNumber++;
-      }
 
-  }
+}
 
-  function markCorrectMatches(guess: string[], word: string[], boxes: NodeListOf<HTMLElement> ) {
-      guess.forEach(function (letter, index, guess) {
-          if (letter === word[index]) {
-                alpha.setCorrect(<Letters>letter);
-              boxes[index].classList.add("correct"); 
-              word[index] = "_";
-              guess[index] = ".";
-          }
-      });
-  }
+// Instantiate guess processor, call its functions in appropriate order?
 
-  function markSemicorrectMatches(guess: string[], word: string[], boxes: NodeListOf<HTMLElement>) {
-      guess.forEach(function (letter, index, guess) {
-          if (word.indexOf(letter) !== -1) {
-                alpha.setSemiCorrect(<Letters>letter); // Casting using <Letters> - this only helps at compile time! Is gone by the time the code is run. This is why casting is bad!
-              boxes[index].classList.add("halfcorrect");
-              word[word.indexOf(letter.toUpperCase())] = "_";
-              guess[index] = ".";
-          }
-          else if (letter !== ".") {
-                alpha.setIncorrect(<Letters>letter);
-              boxes[index].classList.add("incorrect");
-          }
-      });
-  }
+function matchLetters(guess: string, word: string, boxes: NodeListOf<HTMLElement>) {
+
+    console.log(word);
+
+    if (guessNumber >= 6) {
+        inputField.setAttribute("readonly", "");
+        inputField.value = "No more guesses!";
+    }
+
+
+    else {
+
+        let guessArray = guess.toUpperCase().split("");
+        let wordArray = word.toUpperCase().split("");
+
+        markCorrectMatches(guessArray, wordArray, boxes);
+
+        markSemicorrectMatches(guessArray, wordArray, boxes);
+
+        alpha.updateAlphabetDisplay();
+        inputField.value = "";
+        guessNumber++;
+    }
+
+}
+
+function markCorrectMatches(guess: string[], word: string[], boxes: NodeListOf<HTMLElement>) {
+    guess.forEach(function (letter, index, guess) {
+        if (letter === word[index]) {
+            alpha.setCorrect(<Letters>letter);
+            boxes[index].classList.add("correct");
+            word[index] = "_";
+            guess[index] = ".";
+        }
+    });
+}
+
+function markSemicorrectMatches(guess: string[], word: string[], boxes: NodeListOf<HTMLElement>) {
+    guess.forEach(function (letter, index, guess) {
+        if (word.indexOf(letter) !== -1) {
+            alpha.setSemiCorrect(<Letters>letter); // Casting using <Letters> - this only helps at compile time! Is gone by the time the code is run. This is why casting is bad!
+            boxes[index].classList.add("halfcorrect");
+            word[word.indexOf(letter.toUpperCase())] = "_";
+            guess[index] = ".";
+        }
+        else if (letter !== ".") {
+            alpha.setIncorrect(<Letters>letter);
+            boxes[index].classList.add("incorrect");
+        }
+    });
+}
 
